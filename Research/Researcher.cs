854d87bd00936f771c4ce6af4c7dcb93eddcd32e
billz <billzhao1030@gmail.com>
2021-11-18 13:41:53 +1100

    Create packages

---
+++ Research/Researcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAP.Research {
+    class Researcher {
+        public enum Campus {
+            Hobart,
+            Launceston,
+            CradleCoast
+        };
+    }
+}
